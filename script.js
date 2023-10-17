2e342f822bd8d3cf11e4d30dffc5b7dafc316a8d
Suplife <81188670+tonirodolfo@users.noreply.github.com>
2023-10-17 18:25:07 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+  setTimeout(function() {
+    document.getElementById("wallet-scan").style.display = "none";
+    
+    const cryptoList = document.getElementById("crypto-list");
+    cryptoList.style.display = "block";
+    
+    const cryptos = [
+      {name: "BTC", value: 10},
+      {name: "ETH", value: 20},
+      {name: "LINK", value: 5}
+    ];
+    
+    cryptos.forEach(crypto => {
+      const cryptoItem = document.createElement("div");
+      cryptoItem.innerHTML = `<input type="checkbox" id="${crypto.name}" name="${crypto.name}">
+                              <label for="${crypto.name}">${crypto.name} pour $${crypto.value}</label>`;
+      cryptoList.appendChild(cryptoItem);
+    });
+    
+    document.getElementById("swap-button").style.display = "block";
+  }, 3000);
+  
+  document.getElementById("swap-button").addEventListener("click", function() {
+    alert("Swap confirmé !");
+  });
+});
